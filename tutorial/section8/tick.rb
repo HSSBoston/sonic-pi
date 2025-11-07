15a327e7f6b104100ef8d637b0e4605078660bec
hssboston <hanna.s.suzuki@gmail.com>
2025-11-07 16:22:57 -0500

    sec8

---
+++ tutorial/section8/tick.rb
@@ -0,0 +1,11 @@
+##| counter = 0
+##| live_loop :arp do
+##|   play (scale :e3, :minor_pentatonic)[counter], release: 0.1
+##|   counter += 1
+##|   sleep 0.125
+##| end
+
+live_loop :arp do
+  play (scale :e3, :minor_pentatonic).tick, release: 0.1
+  sleep 0.125
+end
