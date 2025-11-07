318025470e03f2dfa670b94cb6ad86c95cd1912b
hssboston <hanna.s.suzuki@gmail.com>
2025-11-07 16:44:12 -0500

    amen

---
+++ amen_break/amen.rb
@@ -0,0 +1,4 @@
+live_loop :amen_break do
+  sample :loop_amen
+  sleep 2
+end
---
+++ amen_break/amen2.rb
@@ -0,0 +1,7 @@
+live_loop :amen_break do
+  use_bpm 60
+  ##| sample :loop_amen, beat_stretch: 2, cutoff: 100
+  sample :loop_amen, beat_stretch: 2
+  sleep 2
+end
+
