f15f614c363305a581abfba5ec3236fb58bdbfcb
hssboston <hanna.s.suzuki@gmail.com>
2025-11-08 11:25:25 -0500

    arpeggio

---
+++ country-roads/chords-test.rb
@@ -0,0 +1,10 @@
+chords =[nil, chord(:f3, :major), chord(:c3, :major),  chord(:d3, :minor),  chord(:bb3, :major),
+         chord(:a3, :minor), chord(:eb3, :major), chord(:bb3, :major), chord(:f3, :major)]
+
+live_loop :chords do
+  with_fx :echo, mix: 0.1 do 
+    c = chords.tick
+    play_chord c, release: 4 if(!c==nil)
+    sleep 4
+  end
+end
---
+++ country-roads/cr-echo-arpeggio.rb
@@ -0,0 +1,53 @@
+use_bpm 70
+use_synth :piano
+
+live_loop :melody do
+  play_pattern_timed [:r, :r, :f4, :g4, :a4, :a4, :g4, :f4, :g4, :a4, :g4, :f4, :a4, :c5, :d5, :d5, :a4],
+                     [2,  1,  0.5, 0.5, 3,   0.25,0.25,0.5, 3,   0.5, 0.5, 3,   0.5, 0.5, 3,   0.5, 0.5]
+  play_pattern_timed [:c5, :c5, :a4, :a4, :g4, :f4, :g4, :a4, :a4, :g4, :f4, :f4, :f4, :g4, :f4],
+                     [0.25, 0.75, 2, 0.25,0.25,0.5, 1,   2,   0.25,0.25,0.5, 3,   0.5, 0.5, 4]
+end
+
+chords =[nil, chord(:f3, :major), chord(:c3, :major),  chord(:d3, :minor),  chord(:bb3, :major),
+              chord(:a3, :minor), chord(:eb3, :major), chord(:bb3, :major), chord(:f3, :major)]
+
+live_loop :chords do
+  with_fx :echo, mix: 0.1 do 
+    sleep 4
+    play_chord , release: 4
+    sleep 4
+    play_chord , release: 4
+    sleep 4
+    play_chord , release: 4
+    sleep 4
+    play_chord , release: 4
+    sleep 4
+    play_chord , release: 4
+    sleep 4
+    play_chord , release: 4
+    sleep 4
+    play_chord , release: 4
+    sleep 4
+    play_chord , release: 4
+  end
+end
+
+live_loop :arppegio do
+  sleep 4
+  play_pattern ring(chord(:f3, :major))
+  ##| sleep 4
+  play_pattern ring(chord(:c3, :major))
+  ##| sleep 4
+  play_pattern ring(chord(:d3, :minor))
+  ##| sleep 4
+  play_pattern ring(chord(:bb3, :major))
+  ##| sleep 4
+  play_pattern ring(chord(:a3, :minor))
+  ##| sleep 4
+  play_pattern ring(chord(:eb3, :major))
+  ##| sleep 4
+  play_pattern ring(chord(:bb3, :major))
+  ##| sleep 4
+  play_pattern chord(:f3, :major)
+end
+
